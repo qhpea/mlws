3ddc733d1c02c7edb1cc0bcd47bd72ca60cca303
lize.live <git@lize.live>
2023-04-10 20:38:19 -0700

    poety2nix still broken

--- flake.nix
+++ flake.nix
@@ -1,30 +1,18 @@
 {
-  description = "mlws";
+  description = "Application packaged using poetry2nix";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    devenv.url = "github:cachix/devenv";
-    nix2container.url = "github:nlewo/nix2container";
-    nix2container.inputs.nixpkgs.follows = "nixpkgs";
-    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.devenv.flakeModule
-        # ./unfree.nix
-        # flake-parts.flakeModules.unfree
-      ];
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-        
-        # set allowUnfree
-        _module.args.pkgs = import nixpkgs {
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
           config.cudaSupport = true;
@@ -33,42 +21,28 @@
           cudaCapabilities = [ "8.6" ];
           # cudaVersion = "12.1";
         };
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
+        poetry2nix' = import poetry2nix {
+          inherit pkgs;
+          # packages.poetry = pkgs.poetry;
+        };
+        customOverrides = self: super: { };
 
-        devenv.shells.default = {
-          name = "mlws";
-          languages.python.enable = true;
-          # python 311 has big performance improvements
-          # languages.python.package = pkgs.python311;
-          languages.python.poetry.enable = true;
-          
-          # need rust quite a bit
-          languages.rust.enable = true;
-          
-          # https://devenv.sh/reference/options/
-          packages = with pkgs; [ cudatoolkit stdenv.cc.cc.lib ];
-
-          # enterShell = ''
-          #   hello
-          # '';
-#            shellHook = ''
-#    export CUDA_PATH=${pkgs.cudatoolkit}
-#    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath (with pkgs; [
-#      linuxPackages.nvidia_x11
-#      cudatoolkit
-#      cudatoolkit.out
-#      cudaPackages.cudnn_8_5_0
-#    ])}"
-#  '';
+        my_env = poetry2nix'.mkPoetryEnv {
+          projectDir = ./.;
+          preferWheels = true;
+          overrides = [ poetry2nix'.defaultPoetryOverrides customOverrides ];
+          python = pkgs.python310;
+        };
+      in
+      {
+        packages = {
+          myapp = poetry2nix'.mkPoetryApplication { projectDir = self; };
+          default = self.packages.${system}.myapp;
         };
 
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-      };
-    };
+        # devShells.default = pkgs.mkShell {
+        #   packages = [ poetry2nix.packages.${system}.poetry ];
+        # };
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ poetry my_env ]; };
+      });
 }
