f409865ee37f7e4b6cc322fcb0e8669f8560a261
lizelive <git@lize.live>
2024-03-22 17:08:58 -0700

    flake

--- flake.nix
+++ flake.nix
@@ -1,32 +1,24 @@
 {
-  description = "Application packaged using poetry2nix";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.poetry2nix = {
-    url = "github:nix-community/poetry2nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgs.${system}.poetry2nix.mkPoetryApplication { projectDir = self; };
+      });
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryApplication;
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          config.cudaSupport = true;
-        };
-      in
-      {
-        packages = {
-          myapp = mkPoetryApplication { projectDir = self; };
-          default = self.packages.${system}.myapp;
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = [ pkgs.poetry ];
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}; [
+            # (poetry2nix.mkPoetryEnv { projectDir = self; })
+            poetry
+          ];
         };
       });
+    };
 }
